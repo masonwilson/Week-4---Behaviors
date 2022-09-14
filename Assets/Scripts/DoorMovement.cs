0057c0f5acd902c14e36074420add5cb03ae53ff
masonwilson <mew8wjn@virginia.edu>
2022-09-14 10:37:31 -0400

    expanded collider area

--- Assets/Scripts/DoorMovement.cs
+++ Assets/Scripts/DoorMovement.cs
@@ -9,6 +9,7 @@ public class DoorMovement : MonoBehaviour
     [SerializeField] float totalMoveDistance;
 
     Vector3 startingLocation;
+    Vector3 endingLocation;
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +31,34 @@ public class DoorMovement : MonoBehaviour
         */
     }
     
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider thing)
     {   
         // i want this to happed only for person (doors are colliding with each other 
 
         float distanceTraveled = GetDistanceTraveled();
-        MoveSide(distanceTraveled, totalMoveDistance);
+        if(thing.tag == "Player")
+        {
+            MoveSide(distanceTraveled, totalMoveDistance);
+        }
+    }
+
+    void OnTriggerStay(Collider thing)
+    {
+        float distanceTraveled = GetDistanceTraveled();
+        if (thing.tag == "Player")
+        {
+            MoveSide(distanceTraveled, totalMoveDistance);
+        }
+        endingLocation = gameObject.transform.position;
+    }
+
+    void OnTriggerExit(Collider thing)
+    {
+        float distanceTraveled = GetDistanceTraveledReversed();
+        if (thing.tag == "Player")
+        {
+            MoveBack(distanceTraveled, totalMoveDistance);   
+        }
     }
 
     void MoveSide(float distanceTraveled, float totalMoveDistance)
@@ -46,12 +69,25 @@ public class DoorMovement : MonoBehaviour
 
         }
     }
-    
+
+    void MoveBack(float distanceTraveled, float totalMoveDistance)
+    {
+        if (distanceTraveled <= totalMoveDistance)
+        {
+            gameObject.transform.Translate(-moveDirection * moveSpeed);
+
+        }
+    }
+
 
     float GetDistanceTraveled()
     {
         return Vector3.Distance(gameObject.transform.position, startingLocation);
     }
 
+    float GetDistanceTraveledReversed()
+    {
+        return Vector3.Distance(gameObject.transform.position, endingLocation);
+    }
 
 }
